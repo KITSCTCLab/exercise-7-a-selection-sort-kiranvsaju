d4e14e439e2abe45b5b6d19303562ab8771ac5cb
kiranvsaju <91770235+kiranvsaju@users.noreply.github.com>
2022-09-15 21:28:30 +0530

    commit 1

--- Main.py
+++ Main.py
@@ -2,6 +2,13 @@ from typing import List
 
 def selectionSort(array, size) -> List[int]:
   # Write your code here
+  for point in range (size)
+  check = point
+  for sort in (point,size):
+    if array[sort]<array[point]:
+      array[sort],array[point] = array[point],array[sort]
+      return array
+  
 
 # Do not change the following code
 input_data = input()
